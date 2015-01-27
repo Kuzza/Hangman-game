811bd9df3ce87e36e1a481baa69342807070c07d
Erika Montani <erika.montani29@gmail.com>
2015-01-27 23:10:33 +0100

    added template

--- game.py
+++ game.py
@@ -8,6 +8,16 @@ word_list = ["amsterdam", "berlin", "london", "madrid", "paris"]
 id = 0
 games = {}
 words = {}
+guessing_letters = {}
+tpl = '''    
+        <p>Game: {{game}}</p>
+        <form action="/games/{{id}}" method="post">
+            <p>Word to guess:   {{guessing_word}}</p>
+            <input type="text" name="char" />
+            <input value="Guess" type="submit" />
+        </form>
+        <p>
+    ''' 
 
 def get_hidden_word(word, letters):
     hw = copy(word)
@@ -23,47 +33,37 @@ def start():
     id += 1
     word = choice(word_list) 
     guessing_word = "."*len(word)
-    guessed_letters = []   
     
     games[id] = {'tries_left': 11, 
                  'status': 'busy',
                  'guessing_word': guessing_word,
-                 'guessing_letters': guessed_letters
                  }
     words[id] = word
-    return '''
-    <p>New Hagman game!!<p><br>
-    <p>Word to guess:   %s</p>
-    <form action='games/%s' method='get'>
-    <input value="Try to guess the word!" type="submit">
-    </form>
-    
-    '''    %(guessing_word, id)
+    guessing_letters[id] = []    
+    return template('''
+        <p>New Hagman game!!<p><br>
+        <p>Word to guess:  {{guessing_word}}</p>
+        <form action='games/{{id}}' method='get'>
+            <input value="Try to guess the word!" type="submit">
+        </form>  
+        ''',  guessing_word=guessing_word, id=id)
 
 @app.route('/games', method='GET')
 def overview():
-    return '''
-        <p>Games: %s</p>
+    return  template('''
+        <p>Games: {{games}}</p>
         <form action="/games" method="post">
             <input value="Start Hangman Game now!" type="submit" />
         </form>
         <p>
-    '''  %games
+        ''', games=games)
 
 @app.route('/games/<id>', method='GET')
 def guess(id):
     id = int(id)
     game = games[id]
     guessing_word = game["guessing_word"]
-    return '''    
-        <p>Game: %s</p>
-        <form action="/games/%s" method="post">
-            <p>Word to guess:   %s</p>
-            <input type="text" name="char" />
-            <input value="Guess" type="submit" />
-        </form>
-        <p>
-    '''  %(game, id, guessing_word)
+    return template(tpl, game=game, id=id, guessing_word=game['guessing_word'])         
 
 
 @app.route('/games/<id>', method='POST')
@@ -72,29 +72,22 @@ def play(id):
     char = request.forms.get('char')
     game = games[id]
     word = words[id]
-    letters = game['guessing_letters']
+    letters = guessing_letters[id]
     if len(char) == 1:
         if char not in letters:
             letters.append(char)    
         if char in word:
             game['guessing_word'] = get_hidden_word(word, letters)
-            if "." not in word:
-                status =  'success!!!'
+            if "." not in game['guessing_word']:
+                game['status'] = 'succes!!!' 
+                return "GAME %s SUCCESSFULLY FINISHED" %id
         else:
             game['tries_left'] -= 1
             if game['tries_left'] == 0:
                 game['status'] = 'failed!!!' 
                 return "GAME %s OVER!" %id
             
-    return '''    
-        <p>Game: %s</p>
-        <form action="/games/%s" method="post">
-            <p>Word to guess:   %s</p>
-            <input type="text" name="char" />
-            <input value="Guess" type="submit" />
-        </form>
-        <p>
-    '''  %(game, id, game['guessing_word'])            
+    return template(tpl, game=game, id=id, guessing_word=game['guessing_word'])            
         
     
 
